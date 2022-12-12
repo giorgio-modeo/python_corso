e1553072c66adb94affd9005188ea772d5a126ac
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2022-12-12 16:37:58 +0100

    Create imput.py

---
+++ imput.py
@@ -0,0 +1,15 @@
+def inp(st,n1,n2):
+    while True:
+        try:
+            numero=int(input(st))
+            if numero <= n1 and numero <= n2:
+                break
+            else:
+                print("il numero non e nel intervallo")
+        except:
+            print("type error")
+    return(numero)
+
+a = inp("inserisci altezza",50,220)
+b=inp("inserisci largezza",10,50)
+
