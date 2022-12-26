dcfaffaf5c46ad4a3b2c3706b0a47fca97e8b8be
giorgio-modeo <dgg.vm3@gmail.com>
2022-12-26 02:19:18 +0100

    Update tari.py

--- tari.py
+++ tari.py
@@ -1,19 +1,25 @@
 #modeo giorgio
-#((mq * tariffa fissa) + (persone + tariffa variabile))+5%
-def f_menu():   
-    m = """ 
-        1. Inserimento dati
-        2. Stampa informazioni TARI
-        3. Uscita
-    """
-    print(m)
-    menu = input(":")
-    match menu:
-    case 1:
-    tari
 
-tari = []
-tari.append(input(""))
+#((mq * tariffa fissa) + (persone + tariffa variabile))+5%
+
+
+m = """ 
+
+    1. Inserimento dati
+
+    2. Stampa informazioni TARI
+
+    3. Uscita
+"""
+print(m)
+
+menu = input(":")
+match menu:
+   case  1:
+
+
+
+
 
 
 
