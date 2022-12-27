8b64d130d402c06de2fdf56e60f215dce96a422a
giorgio-modeo <dgg.vm3@gmail.com>
2022-12-27 16:11:48 +0100

    a

--- tari_diz.py
+++ tari_diz.py
@@ -2,7 +2,7 @@
 # quota fissa + quota variabile 
 #  m    € a persona     tot     tar fissa 
 # 100 * 1,23764     = 123,764 + 49,92488
-
+import os
 tari={
     'abit_uso': {
         1:[1.23764,49.92488],
@@ -38,18 +38,15 @@ due = """
 inp=[]
 utilizzata = tari['abit_uso']
 affitto = tari['all_disp']
-
+i=0
 
     
-def main():
-    ins_d()
-    
-def ins_d():  
-    C_inp(f"selezziona la funzione che vuoi utilizzare{due}: ")
-    f_menu()
-
-
-
+def main():  
+    while True:
+        os.system("cls")
+        del inp[:]
+        C_inp(f"selezziona la funzione che vuoi utilizzare{due}: ")
+        f_menu()
 
     
 def C_inp(st):
@@ -77,46 +74,66 @@ def categoria():
         match inp[1]:
             case 1:
                 print('hai selezzionato "abitazzione in uso"')
-                print("inserisci i resitenti nell'immobile: ")     
+                print("inserisci il numero di resitenti nell'immobile: ")     
                 C_inp("")
                 print("inserisci i metri quadrati della casa")
                 C_inp("")
-                print(inp)
-                comp_fam(inp[3]) 
+                #abitazzione in uso 1 residente 20 mq 
+                """
+                24.7528€ di tassa variabile    
+                74.67768€ di totale
+                70.9438€ il totale scontato 
+                """
+                comp_fam(utilizzata,utilizzata,inp[3],) 
             case 2:
                 print('hai selezzionato "alloggi a disposizzione"')
-                print('hai selezzionato "abitazzione in uso"')
+
                 print("inserisci i resitenti nell'immobile: ")     
                 C_inp("")
                 print("inserisci i metri quadrati della casa")
                 C_inp("")
-                print(inp)
-                comp_fam(inp[3]) 
+                print(affitto,inp)
+                comp_fam(affitto,affitto,inp[3]) 
             case _:
                 print('hai selezzionato "box privati"')
                 
                 
 #                                                                                   componenti famiglia
-def comp_fam(mq):
+def comp_fam(n1,n2,mq):
     match inp[2]:
         case 1:
-            calc_T(utilizzata[1][0],utilizzata[1][1],mq)
+            calc_T(n1[1][0],n2[1][1],mq)
         case 2:
-            calc_T(utilizzata[2][0],utilizzata[2][1],mq)
+            calc_T(n1[2][0],n2[2][1],mq)
         case _:
-            calc_T(utilizzata[3][0],utilizzata[3][1],mq)
+            calc_T(n1[3][0],n2[3][1],mq)
+
 
 
 
 def calc_T(n1,n2,n3):
-    tar_fissa = (n3*n1)
-    tari.update([('tot_1' , tar_fissa + n2)])
-    print(tari)
-    return tar_fissa
+    global i
+    tari.update([(f'tot_{i}' , [n3*n1])])
+    tari[f'tot_{i}']+=[tari[f'tot_{i}'][0] + n2]
+    tari[f'tot_{i}']+=[tari[f'tot_{i}'][1]-((tari[f'tot_{i}'][1]*5)/100)]
+    i =+ 1
+    
 
-x = calc_T 
-def info(x):
-    print(x)
+
+
+
+def info():
+    try:
+        print(i,tari)
+        t_var_r = round(tari['tot_0'][0],5)
+        tot_r = round(tari['tot_0'][1],5)
+        tot_s_r = round(tari['tot_0'][2],5)
+        print(f"{t_var_r}€ di tassa variabile \n{tot_r}€ di totale \n{tot_s_r}€ il totale scontato")
+        input("premere invio per continuare...")
+    except:
+        print("attenzione inserire prima i dati")
+        C_inp(uno)
+        categoria()
 main()
 
 
